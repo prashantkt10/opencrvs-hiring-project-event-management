c40b7f970d9b64de4bafcf139e1495ad335d9a72
Prashant Tripathi <prashantkt10@gmail.com>
2024-09-17 19:59:25 +0530

    login signup home pages setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/index';
+import Login from './pages/Login/index';
+import SignUp from './pages/SignUp/index';
+
+const App: React.FC = () => {
 
   return (
     <>
-    <h1>Hello World</h1>
+      <h1>Event App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+const SignUp: React.FC = () => {
+    return (
+        <div>
+            <h1>SignUp</h1>
+        </div>
+    )
+}
+
+export default SignUp;
